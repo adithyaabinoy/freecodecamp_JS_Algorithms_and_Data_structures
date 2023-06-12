199b1f201421ccfd22cba8c4a0c3b245f67898b3
adithyaabinoy <adithyaabinoy@gmail.com>
2023-06-12 19:44:27 +0530

    basicjs

---
+++ BASIC JAVASCRIPT/52-Local Scope and Functions.js
@@ -0,0 +1,11 @@
+function myLocalScope() {
+    var myVar = 3;
+    // Only change code below this line
+  
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
--- BASIC JAVASCRIPT/58-Use Conditional Logic with If Statements.js
+++ BASIC JAVASCRIPT/58-Use Conditional Logic with If Statements.js
@@ -1,9 +1,9 @@
 function trueOrFalse(wasThatTrue) {
     // Only change code below this line
   if (wasThatTrue) {
-      return "Yes, that was true";
-    }
-    return "No, that was false";
+    return "Yes, that was true";
+  }
+  return "No, that was false";
   
     // Only change code above this line
   
