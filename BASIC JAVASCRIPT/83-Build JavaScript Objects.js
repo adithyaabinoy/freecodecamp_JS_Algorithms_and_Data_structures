e7088ca103f5154d778810a73ec6c7cd80cd5122
adithyaabinoy <adithyaabinoy@gmail.com>
2023-06-13 20:34:59 +0530

    basic_js

---
+++ BASIC JAVASCRIPT/83-Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+  "name": "Casper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["jerry", "robin"]
+  
+    // Only change code above this line
+  };
