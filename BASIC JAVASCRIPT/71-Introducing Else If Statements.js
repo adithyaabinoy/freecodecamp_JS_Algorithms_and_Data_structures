61051da4b03f0ffdb4eaec55cdedbe69459eb2cd
adithyaabinoy <adithyaabinoy@gmail.com>
2023-06-13 14:08:39 +0530

    Basic_js

---
+++ BASIC JAVASCRIPT/71-Introducing Else If Statements.js
@@ -0,0 +1,9 @@
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    } else if (val < 5) {
+        return "Smmaler than 5";
+    } else {
+        return "Between 5 and 10";
+    }
+}
