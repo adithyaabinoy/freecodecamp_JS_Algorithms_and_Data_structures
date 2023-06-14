a7dea27a63a0b30a352b87f7d78b023132ba85c2
adithyaabinoy <adithyaabinoy@gmail.com>
2023-06-14 13:37:38 +0530

    basic_js

---
+++ BASIC JAVASCRIPT/92-Manipulating Complex Objects.js
@@ -0,0 +1,22 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Harry Styles",
+      "title": "Fine Line",
+      "release_year": 2021,
+      "formats": [
+        "CD",
+        "Youtube Video"
+      ]
+    }
+  ];
