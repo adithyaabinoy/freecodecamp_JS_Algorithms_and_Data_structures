0e9f099ea2f3d65f56bf6076de7d55c49342cd7b
adithyaabinoy <adithyaabinoy@gmail.com>
2023-12-08 15:40:00 +0530

    FUNCTIONAL PROGRAMMING

---
+++ FUNCTIONAL PROGRAMMING/Remove Elements from an Array Using slice Instead of splice.js
@@ -0,0 +1,8 @@
+// Rewrite the function nonMutatingSplice by using slice instead of splice. It should limit the provided cities array to a length of 3, and return a new array with only the first three items.
+
+// Do not mutate the original array provided to the function.
+
+function nonMutatingSplice(cities,) {
+
+    return cities.slice(0,3);
+  }
