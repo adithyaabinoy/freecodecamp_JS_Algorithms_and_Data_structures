cfa296aebe42a47356c23040f6a6cabe54638f6b
adithyaabinoy <adithyaabinoy@gmail.com>
2023-12-08 15:27:03 +0530

    FUNCTIONAL PROGRAMMING

--- FUNCTIONAL PROGRAMMING/Return Part of an Array Using the slice Method.js
+++ FUNCTIONAL PROGRAMMING/Return Part of an Array Using the slice Method.js
@@ -1,3 +1,5 @@
+//Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices. The function should return an array.
+
 function sliceArray(anim, beginSlice, endSlice) {
 return anim.slice(beginSlice, endSlice)
 }
