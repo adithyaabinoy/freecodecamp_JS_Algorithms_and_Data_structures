d9bf62e9bf6352d2b478424526a3f14a6581137a
adithyaabinoy <adithyaabinoy@gmail.com>
2023-12-08 10:47:07 +0530

    Basic Algorithm Scripting

---
+++ FUNCTIONAL PROGRAMMING/Pass Arguments to Avoid External Dependence in a Function.js
@@ -0,0 +1,12 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(a) {
+return a + 1;
+
+  // Only change code above this line
+}
+console.log(fixedValue)
+const b = incrementer(fixedValue)
+console.log(b)
